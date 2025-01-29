f625f836c56bdcd320e39c69853a40cc2cb6834a
Jesus Collado <jcoltoc899@g.educaand.es>
2025-01-29 14:32:13 +0100

    Añadiendo los DTOs de MAscota y Veterinario

--- src/main/java/Modelos/MascotaDTO.java
+++ src/main/java/Modelos/MascotaDTO.java
@@ -1,13 +1,71 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelos;
 
-/**
- *
- * @author jesus
- */
+import java.util.Date;
+
 public class MascotaDTO {
-    
+
+    private int id;
+    private String numeroChip;
+    private String nombre;
+    private double peso;
+    private Date fechaNacimiento;
+    private String tipo;
+    private int idVeterinario; // Relacion con Veterinario
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNumeroChip() {
+        return numeroChip;
+    }
+
+    public void setNumeroChip(String numeroChip) {
+        this.numeroChip = numeroChip;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getIdVeterinario() {
+        return idVeterinario;
+    }
+
+    public void setIdVeterinario(int idVeterinario) {
+        this.idVeterinario = idVeterinario;
+    }
 }
--- src/main/java/Modelos/VeterinarioDTO.java
+++ src/main/java/Modelos/VeterinarioDTO.java
@@ -1,13 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Modelos;
 
-/**
- *
- * @author jesus
- */
 public class VeterinarioDTO {
-    
+
+    private int id;
+    private String nif;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String email;
+
+    // Getters y setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNif() {
+        return nif;
+    }
+
+    public void setNif(String nif) {
+        this.nif = nif;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
